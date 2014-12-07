bde50ce727f1aeb77c71f6e0d93e39ad165bcec7
Shoamf NiphraDil <niphradil@gmail.com>
2014-12-07 14:48:37 +0100

    Added contact form

---
+++ example.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shirts 4 Shoamf</title>
+    <?php date_default_timezone_set('Europe/Amsterdam')?>
+</head>
+<body>
+    <h1>Shirts 4 Shoamf</h1>
+    <p>&copy;<?php echo date("Y"); ?> Shirts 4 Shoamf</p>
+</body>
+</html>
